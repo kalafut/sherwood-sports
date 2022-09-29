363b708584c1928f25e8a0284a8d23e75147a15a
Jim Kalafut <jim@kalafut.net>
2022-09-29 10:02:07 -0700

    Update welcome text

--- src/lib/WelcomeBanner.svelte
+++ src/lib/WelcomeBanner.svelte
@@ -18,8 +18,9 @@
     </p>
 
     <p>
-      The site just launched, so data is still be added and little bugs worked
-      out. If you have issues or questions, please
+      The site is still in development, but it's ready for public use. Skim
+      through the <a href="#about">About</a> page for an intro, or just browse
+      around! If you have issues or questions, please
       <a href="#contribute">reach out</a>.
     </p>
     Enjoy!
