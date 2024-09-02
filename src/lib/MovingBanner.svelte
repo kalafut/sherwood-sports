8009a54303b0d82c86ca9706753fa72d259ea3fd
Jim Kalafut <jim@kalafut.net>
2024-09-02 14:16:48 -0700

    Remove moving banner

--- src/lib/MovingBanner.svelte
+++
@@ -1,4 +0,0 @@
-<div id="welcome-alert" class="fs-5 alert alert-danger fade show" role="alert">
-  This site is moving and the <b>sherwoodsports.org</b> domain will be retired soon. The new location is:
-  <a href="https://sports.kalafut.net">sports.kalafut.net</a>
-</div>
